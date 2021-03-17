6a08749875c8347b57450757e8cb8a9923a78dc4
Lev Novikov <lev@learn.ventures>
2021-03-16 23:28:18 -0400

    add: package files

---
+++ src/__about__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""pytorch implementation of trDesign"""
+
+__url__ = "https://github.com/Learn-Ventures/trdesign-pytorch"
+__version__ = "0.0.1"
+__pubdate__ = ""  # date/time in UTC
+__author__ = "Learn Ventures Inc."
+__email__ = "infra@learn.ventures"
+__copyright__ = "Copyright 2021 by Learn Ventures"
+__license__ = "MIT"
---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""pytorch implementation of trDesign"""
+
+# pkg
+from .__about__ import (
+    __version__,
+    __pubdate__,
+    __author__,
+    __email__,
+    __copyright__,
+    __license__,
+)
